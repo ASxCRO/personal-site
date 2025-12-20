1170c6dcb21db53eeb6b051d8ecdf36a671a22df
ASxCRO <antonio.suups@gmail.com>
2025-12-20 16:26:53 +0100

    docs to root

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as About } from "./about/About";
-export { default as Contact } from "./contact/Contact";
-export { default as Experience } from "./experience/Experience";
-export { default as Footer } from "./footer/Footer";
-export { default as Header } from "./header/Header";
-export { default as Nav } from "./nav/Nav";
-export { default as Testimonials } from "./testimonials/Testimonials";
