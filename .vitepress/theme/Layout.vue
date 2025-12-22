b364d1889e61cb0d67fd54e067045ede4d5bf5d8
ASxCRO <antonio.suups@gmail.com>
2025-12-22 23:20:25 +0100

    path

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -1,41 +1,42 @@
 <script setup lang="ts">
-import { useData, useRoute } from 'vitepress'
-import { computed } from 'vue'
-import Nav from './components/Nav.vue'
-import Footer from './components/Footer.vue'
-import BlogPost from './components/BlogPost.vue'
+import { useData, useRoute } from "vitepress";
+import { computed } from "vue";
+import Nav from "./components/Nav.vue";
+import Footer from "./components/Footer.vue";
+import BlogPost from "./components/BlogPost.vue";
 
-const { frontmatter } = useData()
-const route = useRoute()
+const { frontmatter } = useData();
+const route = useRoute();
 
 // Check if current page is a blog post (in /blog/ directory and not the index)
 const isBlogPost = computed(() => {
-  return route.path.startsWith('/blog/') && route.path !== '/blog/'
-})
+	console.log("Route Path:", route.path);
+	return route.path.startsWith("/blog/") && route.path !== "/blog/";
+});
 </script>
 
 <template>
-  <div class="layout">
-    <Nav />
+	<div class="layout">
+		<Nav />
 
-    <!-- Blog post layout -->
-    <template v-if="isBlogPost">
-      <BlogPost>
-        <Content />
-      </BlogPost>
-    </template>
+		<!-- Blog post layout -->
+		<template v-if="isBlogPost">
+			<BlogPost>
+				<Content />
+			</BlogPost>
+		</template>
 
-    <!-- Regular page layout -->
-    <template v-else>
-      <Content />
-    </template>
+		<!-- Regular page layout -->
+		<template v-else>
+			<Content />
+		</template>
 
-    <Footer />
-  </div>
+		<Footer />
+	</div>
 </template>
 
 <style scoped>
 .layout {
-  min-height: 100vh;
+	min-height: 100vh;
 }
 </style>
